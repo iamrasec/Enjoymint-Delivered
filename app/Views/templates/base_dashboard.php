318e7d6d6e3b9170bcabb846517880ba22a505d5
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-07-25 10:51:31 +0800

    Updated Robots Meta tag

--- app/Views/templates/base_dashboard.php
+++ app/Views/templates/base_dashboard.php
@@ -5,6 +5,7 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="atoken" content="<?php echo $user_jwt; ?>">
+  <meta name="robots" content="<?= getenv('CRAWL_META'); ?>">
   <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url(); ?>/assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="<?php echo base_url(); ?>/assets/img/favicon.png">
   <title>
