f6d36efd4cc7c00521d698008c78b611848f4a25
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-07-28 01:46:28 +0800

    back to uppercase

--- app/Models/userModel.php
+++ app/Models/userModel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use CodeIgniter\Model;
 
-class userModel extends Model {
+class UserModel extends Model {
   protected $table = 'users';
   protected $allowedFields = ['guid', 'first_name', 'last_name', 'email', 'role', 'password', 'updated_at'];
   protected $beforeInsert = ['beforeInsert'];
