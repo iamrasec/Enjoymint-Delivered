0beb3cdcec9ad4e80cc0e4f6bf5c8f0e9c8d1cd2
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-08-22 05:54:01 +0800

    signin

--- app/Views/login.php
+++ app/Views/login.php
@@ -2,21 +2,22 @@
 
 <?php $this->section("content") ?>
 
-<?php echo $this->include('templates/__navigation.php'); ?>
+<?php // echo $this->include('templates/__navigation.php'); ?>
 
 
-<main class="main-content mt-0 ps">
+<main class="main-content mt-0 ps login-page">
     <section>
       <div class="page-header min-vh-100">
         <div class="container">
           <div class="row">
             <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column">
-              <div class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" style="background-image: url('../../../assets/img/illustrations/illustration-signin.jpg'); background-size: cover;"></div>
+              <div class="position-relative h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" style="background-image: url('<?= base_url('assets/img/age-check/cannabis.jpg'); ?>'); background-size: cover;"></div>
             </div>
             <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
               <div class="card card-plain">
                 <div class="card-header text-center">
-                  <h4 class="font-weight-bolder">Sign In</h4>
+                  <img src="<?= base_url('assets/img/logo-dark.png'); ?>" style="width: 300px;">
+                  <h4 class="font-weight-bolder mt-5">Sign In</h4>
                   <p class="mb-0">Enter your email and password to sign in</p>
                 </div>
                 <div class="card-body mt-2">
@@ -34,10 +35,10 @@
                       <label class="form-label">Password</label>
                       <input type="password" name="password" class="form-control">
                     </div>
-                    <div class="form-check form-switch d-flex align-items-center mb-3">
+                    <!-- <div class="form-check form-switch d-flex align-items-center mb-3">
                       <input class="form-check-input" type="checkbox" id="rememberMe">
                       <label class="form-check-label mb-0 ms-3" for="rememberMe">Remember me</label>
-                    </div>
+                    </div> -->
                     <div class="text-center">
                       <button type="submit" class="login-btn btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Sign in</button>
                     </div>
