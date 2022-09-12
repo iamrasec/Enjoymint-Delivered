614063085b382deebdf684dc5eaca6bdf4319c99
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-09-12 12:00:59 +0800

    Styled pager.  added area for product filter

--- app/Controllers/Shop.php
+++ app/Controllers/Shop.php
@@ -37,24 +37,27 @@ class Shop extends BaseController
         $this->data['page_title'] = $page_title;
         // $this->data['products'] = $this->product_model->get()->getResult();
 
-           $all_products = $this->product_model->paginate(30);
+           $all_products = $this->product_model->paginate(20);
+
+           // echo "<pre>".print_r($all_products, 1)."</pre>";die();
+
         //  $all_products = $this->product_model->getAllProducts();
         $product_arr = [];
         $count = 0;
         foreach($all_products as $product) {
              $product_arr[$count] = $product;
-            if(!empty($product->images)) {
+            if(!empty($product['images'])) {
                 $imageIds = [];
-                $imageIds = explode(',',$product->images);
+                $imageIds = explode(',',$product['images']);
                 $images = $this->image_model->whereIn('id', $imageIds)->get()->getResult();
-                $product_arr[$count]->images = $images;
+                $product_arr[$count]['images'] = $images;
             }
 
              $count++;
         }
    
 
-        $this->data['products'] = $all_products;
+        $this->data['products'] = $product_arr;
         $this->data['pager'] = $this->product_model->pager;
         $this->data['categories'] = $this->category_model->get()->getResult();
        return view('shop_view', $this->data);
