098f30868672d9e21cd2e98c3d5b3f6661d2fd01
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-09-30 05:21:12 +0800

    Fixed view blog page

--- app/Views/Blogs/view_blog.php
+++ app/Views/Blogs/view_blog.php
@@ -4,51 +4,29 @@
 
 <?php echo view('templates/__navigation.php'); ?>
 
-    <div class="container">
-      <div class="row">
-        <div class="col-12">
-          <div class="card mt-8">
-            <div class="card-body">
-            <div class="col-lg-12 col-sm-12 mt-5 text-center">
-              <h1>Blog Details</h1>
-            </div>
-                <form  role="form" method="post" action="/blogs">
-                 <div class="row"> 
-                  <?php foreach($blogs as $blg):?> 
-                    <div class="col-md-6 col-sm-6 pt-4 pb-4">
-                    <div class="product-featured">
-                    <div class="img-wrap">
-                      <img class="prod_image" src="<?= base_url($blg->url); ?>" alt="No image" style="width:290px;" />
-                  </div>   
-                </div>
-                </div>
-                <div class="col-md-6 col-sm-6 pt-4 pb-4">
-                  <div class="product-featured">
-                  <div class="img-wrap">
-                    <h3><?= $blg->title; ?></h3>                                   
-                    <div class="row mb-5">
-                      <div class="col-12 col-sm-12">
-                      <?= $blg->content; ?>
-                      </div>
-                   </div>
-                   <div class="row mb-5">
-                      <div class="col-12 col-sm-12">
-                        <h7><span class="price d-inline"><?= $blg->author; ?></span></h7>
-                      </div>
-                    </div>   
-                </div>
-                   </div>   
-                </div>
-                <?php endforeach; ?>     
-              </div>         
-            </div> 
-              
-         
-
+<div class="container">
+  <div class="row">
+    <div class="col-12">
+      <div class="card mt-8">
+        <div class="card-body">
+          <div class="row">
+            <div class="col-lg-12 col-sm-12 mt-0 text-left">
+              <img src="<?= base_url('blogs/images/'.$blog[0]->images[0]->filename); ?>">
+              <h1 class="blog-title mt-3"><?= $blog[0]->title; ?></h1>
             </div>
           </div>
-        </div>
+
+          <div class="row">
+            <div class="col-lg-12 col-sm-12 mt-3">
+              <?= $blog[0]->content; ?>
+            </div>
+          </div>
+                    
+        </div> 
       </div>
+    </div>
+  </div>
+</div>
 <style>
   h1{
     margin-top: -40px;
