4b2f06979d319b047e1c60300da54f0260c9ed76
Welyelf <welyelfhisula@gmail.com>
2022-10-01 02:21:22 +0800

    feat: Add Experience Page

--- app/Models/ExperienceModel.php
+++ app/Models/ExperienceModel.php
@@ -8,4 +8,28 @@ class ExperienceModel extends Model {
   protected $table = 'experience';
   protected $allowedFields = ['name', 'url'];
 
+/** 
+   * Get Products in a Particular Experience
+   * 
+   */
+ public function experienceGetAllProduct($exp_id){
+  $this->select('products.*, compounds.thc_unit, compounds.thc_value, compounds.cbd_unit, compounds.cbd_value, experience.url');
+  $this->join('product_experience', 'product_experience.exp_id = experience.id', 'inner');
+  $this->join('products', 'products.id = product_experience.pid', 'inner');
+  $this->join('compounds', 'compounds.pid = products.id', 'left');
+  $this->where('experience.id', $exp_id);
+  return $this->get()->getResult();
+
+ }
+
+ public function experienceGetProductsPaginate($exp_id) {
+  $this->select('products.*, compounds.thc_unit, compounds.thc_value, compounds.cbd_unit, compounds.cbd_value');
+  $this->join('product_experience', 'product_experience.exp_id = experience.id', 'inner');
+  $this->join('products', 'products.id = product_experience.pid', 'inner');
+  $this->join('compounds', 'compounds.pid = products.id', 'left');
+  $this->where('experience.id', $exp_id);
+  return $this->paginate(28);
+
+}
+ 
 }
