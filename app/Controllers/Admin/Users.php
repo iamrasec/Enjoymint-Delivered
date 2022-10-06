5a34bb71783e4094005eec4611e60012933217a5
Welyelf Hisula <32445227+Welyelf@users.noreply.github.com>
2022-10-07 02:46:27 +0800

    feat: users list

---
+++ app/Controllers/Admin/Users.php
@@ -0,0 +1,102 @@
+<?php
+namespace App\Controllers\Admin;
+use App\Controllers\BaseController;
+
+class Users extends BaseController {
+
+  public function __construct() {
+    helper(['jwt']);
+
+    $this->data = [];
+    $this->role = session()->get('role');
+    $this->isLoggedIn = session()->get('isLoggedIn');
+    $this->guid = session()->get('guid');
+    $this->user_model = model('UserModel');
+
+    $this->data['user_jwt'] = getSignedJWTForUser($this->guid);
+
+    if($this->isLoggedIn !== 1 && $this->role !== 1) {
+      return redirect()->to('/');
+    }
+    
+  }
+  
+  public function index() 
+  {
+    // $data = [];
+    $page_title = 'List User';
+
+    $this->data['page_body_id'] = "user_list";
+    $this->data['breadcrumbs'] = [
+      'parent' => [],
+      'current' => $page_title,
+    ];
+    $this->data['page_title'] = $page_title;
+    $this->data['users'] = $this->user_model->get()->getResult();
+    return view('User/list_user', $this->data);
+  }
+
+  public function add_user() 
+  {
+      $page_title = 'Add User';
+      $this->data['page_body_id'] = "User_list";
+      $this->data['breadcrumbs'] = [
+        'parent' => [
+          ['parent_url' => base_url('/admin/users'), 'page_title' => 'Users'],
+        ],
+        'current' => $page_title,
+      ];
+      $this->data['page_title'] = $page_title;
+      echo view('User/add_user', $this->data);
+  }
+
+  /**
+   * This function will fetch product list from post request of datatable server side processing
+   * 
+   * @return json product list json format
+  */
+  public function getUserLists()
+  {
+    $data  = array();
+    $start = $_POST['start'];
+    $length = $_POST['length'];
+
+    $users = $this->user_model->select('*')
+      ->like('first_name',$_POST['search']['value'])
+      ->orLike('last_name',$_POST['search']['value'])
+      ->limit($length, $start)
+      ->get()
+      ->getResult();
+   
+    foreach($users as $user){
+      $start++;
+      if($user->role == '1'){
+        $roles = 'Admin';
+      }
+      elseif($user->role == '3'){
+        $roles = 'Costumers';
+      }
+      else{
+        return '';
+      }
+      $name = $user->first_name.' '.$user->last_name;
+      $data[] = array(
+        $user->id, 
+        $name, 
+        $roles,
+        "<a href=".base_url().">Delete</a> | <a href=".base_url().">edit</a>",
+      );
+    }
+
+    $output = array(
+      "draw" => $_POST['draw'],
+      "recordsTotal" => $this->user_model->countAll(),
+      "recordsFiltered" => $this->user_model->countAll(),
+      "data" => $data,
+    );
+    
+    echo json_encode($output);
+
+  }
+
+}
---
+++ app/Views/User/list_user.php
@@ -0,0 +1,80 @@
+<?php $this->extend("templates/base_dashboard"); ?>
+
+<?php $this->section("content") ?>
+
+<?php echo $this->include('templates/__dash_navigation.php'); ?>
+
+<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+  <!-- Navbar -->
+  <?php echo $this->include('templates/__dash_top_nav.php'); ?>
+  <!-- End Navbar -->
+  
+  <div class="container-fluid py-4">
+    <div class="row">
+      <div class="col-lg-6">
+        <h4><?php echo $page_title; ?></h4>
+      </div>
+      <div class="col-lg-6 text-right d-flex flex-column justify-content-center">
+        <a class="btn bg-gradient-primary mb-0 ms-lg-auto me-lg-0 me-auto mt-lg-0 mt-2" href="<?php echo base_url('/users/add_user'); ?>">Add User</a>
+      </div>
+    </div>
+
+    <div class="row mt-4">
+      <div class="col-lg-12 mt-lg-0 mt-4">
+        <div class="card">
+          <div class="card-body">
+            <div class="table-responsives">
+              <table id="blog-table" class="table align-items-center mb-0">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tfoot>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>Role</th>
+                    <th>Action</th>
+                </tfoot>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php $this->endSection(); ?>
+
+<?php $this->section('scripts'); ?>
+<!-- Load Data Table JS -->
+<script src="<?= base_url('assets/js/plugins/jquery.dataTables.min.js') ?>"></script>
+
+<!-- Product List page js -->
+<script>
+  $(document).ready(function () {
+      $('#blog-table').DataTable({
+        // Processing indicator
+        "processing": true,
+        // DataTables server-side processing mode
+        "serverSide": true,
+        // Initial no order.
+        "order": [],
+        // Load data from an Ajax source
+        "ajax": {
+            "url": "<?= base_url('admin/users/getUserLists'); ?>",
+            "type": "POST"
+        },
+        //Set column definition initialisation properties
+        "columnDefs": [{ 
+            "targets": [0],
+            "orderable": false
+        }]
+    });
+  });
+</script>
+<?php $this->endSection(); ?>
