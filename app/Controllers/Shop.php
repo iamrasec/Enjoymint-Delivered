122f9386255d6d6c4dc72d8a32d9d17892c3a858
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-10-08 10:04:33 +0800

    Fast tracked

--- app/Controllers/Shop.php
+++ app/Controllers/Shop.php
@@ -112,5 +112,90 @@ class Shop extends BaseController
         $this->data['strains'] = $this->strain_model->orderBy('name', 'ASC')->get()->getResult();
         return view('shop_view', $this->data);
     }
+
+    public function fast_tracked()
+    {
+        $page_title = 'Shop';
+
+        $this->data['page_body_id'] = "shop";
+        $this->data['breadcrumbs'] = [
+        'parent' => [],
+        'current' => $page_title,
+        ];
+        $this->data['page_title'] = $page_title;
+        // $this->data['products'] = $this->product_model->get()->getResult();
+        $searchData = $this->request->getGet();
+        // $search = "";
+
+        $this->data['current_filter'] = [];
+
+        // echo "<pre>".print_r($searchData, 1)."</pre>";
+
+        //$all_products = $this->product_model->paginate(30);
+       if(!empty($searchData['page'])){
+        $page = $searchData['page'];
+       }else{
+        $page = null;
+       }
+         if(empty($searchData)){
+            // $all_products = $this->product_model->paginate(30);
+            $all_products = $this->product_model->getAllProducts();
+        }else{
+            $category = $searchData['category'];
+            $min_price = $searchData['min_price'];
+            $max_price = $searchData['max_price'];
+            $strain = $searchData['strain'];
+            $brands = $searchData['brands'];
+            $min_thc = $searchData['min_thc'];
+            $max_thc = $searchData['max_thc'];
+            $min_cbd = $searchData['min_cbd'];
+            $max_cbd = $searchData['max_cbd'];
+            $all_products = $this->product_model->getDataWithParam($category, $min_price, $max_price, $strain, $brands, $min_thc, $max_thc, $min_cbd, $max_cbd);
+
+            // echo "<pre>".print_r($this->product_model->getLastQuery()->getQuery(), 1)."</pre>";
+
+            $current_filter = [
+                'category' => $searchData['category'],
+                'strain' => $searchData['strain'],
+                'brands' => $searchData['brands'],
+                'min_price' => $searchData['min_price'],
+                'max_price' => $searchData['max_price'],
+                'min_thc' => $searchData['min_thc'],
+                'max_thc' => $searchData['max_thc'],
+                'min_cbd' => $searchData['min_cbd'],
+                'max_cbd' => $searchData['max_cbd'],
+            ];
+                
+            $this->data['current_filter'] = $current_filter;
+           
+        }
+        // $all_products = $this->product_model->paginate(30);
+
+        // echo "<pre>".print_r($all_products, 1)."</pre>"; die();
+
+        $product_arr = [];
+        $count = 0;
+        foreach($all_products as $product) {
+            // echo "<pre>".print_r($product, 1)."</pre>";
+             $product_arr[$count] = $product;
+            if(!empty($product['images'])) {
+                $imageIds = [];
+                $imageIds = explode(',',$product['images']);
+                $images = $this->image_model->whereIn('id', $imageIds)->get()->getResult();
+                $product_arr[$count]['images'] = $images;
+            }
+
+             $count++;
+        }
+
+        // echo "<pre>".print_r($product_arr, 1)."</pre>"; die();
+
+        $this->data['products'] = $product_arr;
+        $this->data['pager'] = $this->product_model->pager;
+        $this->data['categories'] = $this->category_model->orderBy('name', 'ASC')->get()->getResult();
+        $this->data['brands'] = $this->brand_model->orderBy('name', 'ASC')->get()->getResult();
+        $this->data['strains'] = $this->strain_model->orderBy('name', 'ASC')->get()->getResult();
+        return view('shop_view', $this->data);
+    }
     
 }
--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -22,6 +22,14 @@ class ProductModel extends Model {
     return $this->get()->getResult();
   }
 
+  public function getFastTracked() {
+    $this->select('products.*, compounds.thc_unit, compounds.thc_value, compounds.cbd_unit, compounds.cbd_value, strains.name AS strain_name, strains.url_slug AS strain_url');
+    $this->join('strains', 'strains.id = products.strain', 'left');
+    $this->join('compounds', 'compounds.pid = products.id', 'left');
+    $this->where('products.delivery_type', 2);
+    return $this->paginate(28);
+  }
+
   public function getProductData($pid) {
     $this->select('products.*, compounds.thc_unit, compounds.thc_value, compounds.cbd_unit, compounds.cbd_value, strains.name AS strain_name, strains.url_slug AS strain_url');
     $this->join('strains', 'strains.id = products.strain', 'left');
