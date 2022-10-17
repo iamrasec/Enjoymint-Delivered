8320b4b48f4dfd012ad829b0bd437288d3d014cc
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-10-18 05:32:12 +0800

    added tawk to script

--- app/Views/templates/base.php
+++ app/Views/templates/base.php
@@ -179,6 +179,20 @@ $(document).ready(function() {
   update_cart_count();
 });
 </script>
+
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/589e8f2957ed180aac13db2d/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
 </body>
 
 </html>
