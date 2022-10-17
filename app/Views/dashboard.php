08459f62dc9158eb63120a7e0cf77897ef6bb389
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-10-18 05:40:35 +0800

    Removed unused top part of the dashboard

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -8,7 +8,7 @@
     <!-- Navbar -->
     <nav class="navbar navbar-main navbar-expand-lg position-sticky mt-4 top-1 px-0 mx-4 shadow-none border-radius-xl z-index-sticky" id="navbarBlur" data-scroll="true">
       <div class="container-fluid py-1 px-3">
-        <nav aria-label="breadcrumb">
+        <!-- <nav aria-label="breadcrumb">
           <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
             <li class="breadcrumb-item text-sm">
               <a class="opacity-3 text-dark" href="javascript:;">
@@ -31,7 +31,7 @@
             <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Sales</li>
           </ol>
           <h6 class="font-weight-bolder mb-0">Sales</h6>
-        </nav>
+        </nav> -->
         <div class="sidenav-toggler sidenav-toggler-inner d-xl-block d-none ">
           <a href="javascript:;" class="nav-link text-body p-0">
             <div class="sidenav-toggler-inner">
@@ -41,7 +41,7 @@
             </div>
           </a>
         </div>
-        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
+        <!-- <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
           <div class="ms-md-auto pe-md-3 d-flex align-items-center">
             <div class="input-group input-group-outline">
               <label class="form-label">Search here</label>
@@ -122,7 +122,7 @@
               </ul>
             </li>
           </ul>
-        </div>
+        </div> -->
       </div>
     </nav>
     <!-- End Navbar -->
