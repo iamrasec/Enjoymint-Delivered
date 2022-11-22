25b694fb26161b42545eff0f6da76ac3bbfeaa47
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-11-22 11:30:33 +0800

    clean-up

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -337,16 +337,6 @@ function enjoymintAlert(title, text, icon, is_reload = 0, redirect)
 // }
 
 function tConvert (time) {
-  // Check correct time format and split into components
-  // time = time.toString ().match (/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
-
-  // if (time.length > 1) { // If time format correct
-  //   time = time.slice (1);  // Remove full string match value
-  //   time[5] = +time[0] < 12 ? 'AM' : 'PM'; // Set AM/PM
-  //   time[0] = +time[0] % 12 || 12; // Adjust hours
-  // }
-  // return time.join (''); // return adjusted time or original string
-
   if(time > 1200) {
     time = time - 1200 + ' PM';
   }
