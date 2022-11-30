350112502862ecb10d34737382c9785309137852
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-12-01 05:32:41 +0800

    footer contents added

--- app/Views/templates/base.php
+++ app/Views/templates/base.php
@@ -44,63 +44,7 @@
 
 <?php echo $this->renderSection("content"); ?>
 
-<footer class="footer pt-5 mt-5">
-  <div class="container">
-    <div class=" row">
-      <div class="col-md-3 mb-4 ms-auto">
-        <div>
-          <img src="/assets/img/Enjoymint-Logo-Landscape-White-2.png" class="logo" />
-        </div>
-        <div>
-          <ul class="d-flex flex-row ms-n3 nav">
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="#" target="_blank">
-                <i class="fab fa-facebook text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="#" target="_blank">
-                <i class="fab fa-twitter text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="#" target="_blank">
-                <i class="fab fa-dribbble text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="#" target="_blank">
-                <i class="fab fa-github text-lg opacity-8"></i>
-              </a>
-            </li>
-
-            <li class="nav-item">
-              <a class="nav-link pe-1" href="#" target="_blank">
-                <i class="fab fa-youtube text-lg opacity-8"></i>
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-      <div class="col-md-2 col-sm-6 col-6 mb-4"></div>
-      <div class="col-md-2 col-sm-6 col-6 mb-4"></div>
-      <div class="col-md-2 col-sm-6 col-6 mb-4"></div>
-      <div class="col-md-2 col-sm-6 col-6 mb-4"></div>
-
-      <div class="col-12">
-        <div class="text-center">
-          <p class="my-4 text-sm font-weight-normal">
-            All rights reserved. Copyright © <script>document.write(new Date().getFullYear())</script> Enjoymint Delivered</a>.
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
+<?php echo $this->include('sections/enjoymint_footer.php'); ?>
 
 <?php echo $this->include('templates/__age_check.php'); ?>
 
