19cd648e47747c84058c91f868334d056fabb5c5
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-12-03 05:33:35 +0800

    homepage fix

--- app/Views/home_view.php
+++ app/Views/home_view.php
@@ -105,7 +105,7 @@
                   <hr id="color" class="mt-0">
                   <p class="price">$<span><?= $products[$countp]['price']; ?></span></p>
                   <hr id="color" class="mt-0">
-                  <button class="btn add-to-cart add-product-<?= $product['id']; ?> btn-md bg-warning text-white" type="button" name="add-to-cart" data-pid="<?= $products[$countp]['id']; ?>">
+                  <button class="btn add-to-cart add-product-<?= $products['id']; ?> btn-md bg-warning text-white" type="button" name="add-to-cart" data-pid="<?= $products[$countp]['id']; ?>">
                     <span class="material-icons">add_shopping_cart</span> Add to Cart
                   </button>
                   <div class="lds-hourglass d-none"></div>
