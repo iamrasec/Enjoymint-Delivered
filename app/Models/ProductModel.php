7a9177f257eadf67500380023aed14521cfae8a1
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-12-07 12:57:46 +0800

    filtered out 0 stocks from popular products result

--- app/Models/ProductModel.php
+++ app/Models/ProductModel.php
@@ -58,6 +58,7 @@ class ProductModel extends Model {
     $this->select('products.*, compounds.thc_unit, compounds.thc_value, compounds.cbd_unit, compounds.cbd_value, strains.name AS strain_name, strains.url_slug AS strain_url');
     $this->join('strains', 'strains.id = products.strain', 'left');
     $this->join('compounds', 'compounds.pid = products.id', 'left');
+    $this->where('products.stocks > 0');
     $this->orderBy('products.orders', 'DESC');
     $this->orderBy('products.views', 'DESC');
     $this->limit($limit);
