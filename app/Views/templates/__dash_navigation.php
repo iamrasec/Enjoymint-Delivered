a5783b3f93683f4ea3a8edd61a7d64bd0574d122
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-12-08 07:39:36 +0800

    updated dashboard home link

--- app/Views/templates/__dash_navigation.php
+++ app/Views/templates/__dash_navigation.php
@@ -4,7 +4,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3   bg-gradient-dark" id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href="/dashboard" target="_blank">
+      <a class="navbar-brand m-0" href="<?= base_url('admin/dashboard'); ?>">
         <img src="/assets/img/Enjoymint-Logo-Landscape-White-2.png" class="sidebar_logo" />
       </a>
     </div>
