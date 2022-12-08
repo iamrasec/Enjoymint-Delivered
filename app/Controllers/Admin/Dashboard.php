4bc21810636692d1744bd2c1fe1c2e568185b4ed
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-12-08 13:56:27 +0800

    dashboard sales card

--- app/Controllers/Admin/Dashboard.php
+++ app/Controllers/Admin/Dashboard.php
@@ -15,10 +15,13 @@ class Dashboard extends BaseController {
     $this->guid = session()->get('guid');
     $this->product_model = model('ProductModel');
     $this->checkout_model = model('CheckoutModel');
+    $this->user_model = model('UserModel');
 
     $this->sender_email = getenv('SMTP_EMAIL_USER');
 
     date_default_timezone_set('America/Los_Angeles');
+
+    $this->today = new Time("now", "America/Los_Angeles", "en_EN");
 
     $this->data['user_jwt'] = ($this->guid != '') ? getSignedJWTForUser($this->guid) : '';
 
@@ -43,29 +46,93 @@ class Dashboard extends BaseController {
     // }
 
     $this->data['page_body_id'] = "user_login";
+    $this->data['daily_sales'] = $this->daily_sales();
+    $this->data['monthly_sales'] = $this->monthly_sales();
+    $this->data['annual_sales'] = $this->annual_sales();
+    $this->data['active_users'] = $this->active_users();
 
-    // $this->daily_sales();die();
+    // $this->active_users();die();
 
     echo view('dashboard', $this->data);     
   }
 
   private function daily_sales() {
-    $yesterday = new Time("yesterday", "America/Los_Angeles", "en_EN");
-    $today = new Time("now", "America/Los_Angeles", "en_EN");
+    $toDate = $this->today->toDateString();
 
-    $yesterDate = $yesterday->toDateString();
-    $toDate = $today->toDateString();
+    $where1 = 'created >= "'. $toDate .' 00:00:00"';
+    $where2 = 'created <= "'. $toDate .' 23:59:59"';
 
-    $debug = [
-      'yesterday' => $yesterday,
-      'today' => $today,
-      'yesterDate' => $yesterDate,
-      'toDate' => $toDate,
+    $raw_data = $this->checkout_model->where($where1)->where($where2)->where('status = 3')->get()->getResult();
+
+    $total_sales = 0;
+    foreach($raw_data as $data) {
+      $total_sales += $data->subtotal;
+    }
+
+    $return = [
+      'date' => $this->today->toLocalizedString('MMM d'),
+      'total_sales' => $total_sales,
     ];
 
-    echo "<pre>".print_r($debug,1)."</pre>";
+    return $return;
+  }
 
+  private function monthly_sales() {
+    $currMonth = $this->today->month;
+    $currYear = $this->today->year;
 
-    // $raw_data = $this->checkout_model->where()
+    $maxDays = date('t'); 
+
+    $startMonth = $currYear.'-'.$currMonth.'-1 00:00:00';
+    $endMonth = $currYear.'-'.$currMonth.'-'.$maxDays.' 23:59:59';
+
+    $where1 = 'created >= "'. $startMonth .' 00:00:00"';
+    $where2 = 'created <= "'. $endMonth .' 23:59:59"';
+
+    $raw_data = $this->checkout_model->where($where1)->where($where2)->where('status = 3')->get()->getResult();    
+
+    $total_sales = 0;
+    foreach($raw_data as $data) {
+      $total_sales += $data->subtotal;
+    }
+
+    $return = [
+      'date' => $this->today->toLocalizedString('MMM YYYY'),
+      'total_sales' => $total_sales,
+    ];
+
+    return $return;
+  }
+
+  private function annual_sales() {
+    $currYear = $this->today->year;
+
+    $startYear = $currYear.'-1-1 00:00:00';
+    $endYear = $currYear.'-12-31 23:59:59';
+
+    $where1 = 'created >= "'. $startYear .' 00:00:00"';
+    $where2 = 'created <= "'. $endYear .' 23:59:59"';
+
+    $raw_data = $this->checkout_model->where($where1)->where($where2)->where('status = 3')->get()->getResult();    
+
+    $total_sales = 0;
+    foreach($raw_data as $data) {
+      $total_sales += $data->subtotal;
+    }
+
+    $return = [
+      'date' => $this->today->toLocalizedString('YYYY'),
+      'total_sales' => $total_sales,
+    ];
+
+    return $return;
+  }
+
+  private function active_users() {
+    $active_users = $this->user_model->where('role = 3')->where('is_active = 1')->countAllResults();
+
+    // print_r($active_users);
+
+    return $active_users;
   }
 }
--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -126,6 +126,7 @@
       </div>
     </nav>
     <!-- End Navbar -->
+    <!-- <pre><?= print_r($daily_sales, 1); ?></pre> -->
     <div class="container-fluid py-3">
       <div class="row">
         <div class="col-sm-3">
@@ -133,22 +134,23 @@
             <div class="card-body p-3 position-relative">
               <di v class="row">
                 <div class="col-7 text-start">
-                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Sales</p>
+                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Daily Sales</p>
                   <h5 class="font-weight-bolder mb-0">
-                    $230,220
+                    $<?= number_format($daily_sales['total_sales'], 2, ".", ","); ?>
                   </h5>
-                  <span class="text-sm text-end text-success font-weight-bolder mt-auto mb-0">+55% <span class="font-weight-normal text-secondary">since last month</span></span>
+                  <!-- <span class="text-sm text-end text-success font-weight-bolder mt-auto mb-0">+55% <span class="font-weight-normal text-secondary">since last month</span></span> -->
                 </div>
                 <div class="col-5">
                   <div class="dropdown text-end">
-                    <a href="javascript:;" class="cursor-pointer text-secondary" id="dropdownUsers1" data-bs-toggle="dropdown" aria-expanded="false">
-                      <span class="text-xs text-secondary">6 May - 7 May</span>
+                    <span class="text-xs text-secondary"><?= $daily_sales['date']; ?></span>
+                    <!-- <a href="javascript:;" class="cursor-pointer text-secondary" id="dropdownUsers1" data-bs-toggle="dropdown" aria-expanded="false">
+                      <span class="text-xs text-secondary"><?= $daily_sales['date']; ?></span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end px-2 py-3" aria-labelledby="dropdownUsers1">
                       <li><a class="dropdown-item border-radius-md" href="javascript:;">Last 7 days</a></li>
                       <li><a class="dropdown-item border-radius-md" href="javascript:;">Last week</a></li>
                       <li><a class="dropdown-item border-radius-md" href="javascript:;">Last 30 days</a></li>
-                    </ul>
+                    </ul> -->
                   </div>
                 </div>
               </div>
@@ -160,55 +162,50 @@
             <div class="card-body p-3 position-relative">
               <div class="row">
                 <div class="col-7 text-start">
-                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Customers</p>
+                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Monthly Sales</p>
                   <h5 class="font-weight-bolder mb-0">
-                    3.200
+                    $<?= number_format($monthly_sales['total_sales'], 2, ".", ","); ?>
                   </h5>
-                  <span class="text-sm text-end text-success font-weight-bolder mt-auto mb-0">+12% <span class="font-weight-normal text-secondary">since last month</span></span>
+                  <!-- <span class="text-sm text-end text-success font-weight-bolder mt-auto mb-0">+12% <span class="font-weight-normal text-secondary">since last month</span></span> -->
                 </div>
                 <div class="col-5">
                   <div class="dropdown text-end">
-                    <a href="javascript:;" class="cursor-pointer text-secondary" id="dropdownUsers2" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span class="text-xs text-secondary"><?= $monthly_sales['date']; ?></span>
+                    <!-- <a href="javascript:;" class="cursor-pointer text-secondary" id="dropdownUsers2" data-bs-toggle="dropdown" aria-expanded="false">
                       <span class="text-xs text-secondary">6 May - 7 May</span>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end px-2 py-3" aria-labelledby="dropdownUsers2">
                       <li><a class="dropdown-item border-radius-md" href="javascript:;">Last 7 days</a></li>
                       <li><a class="dropdown-item border-radius-md" href="javascript:;">Last week</a></li>
                       <li><a class="dropdown-item border-radius-md" href="javascript:;">Last 30 days</a></li>
-                    </ul>
+                    </ul> -->
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
         <div class="col-sm-3 mt-sm-0 mt-3">
           <div class="card">
             <div class="card-body p-3 position-relative">
               <div class="row">
                 <div class="col-7 text-start">
-                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Avg. Revenue</p>
+                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Annual Sales</p>
                   <h5 class="font-weight-bolder mb-0">
-                    $1.200
+                    $<?= number_format($annual_sales['total_sales'], 2, ".", ","); ?>
                   </h5>
-                  <span class="font-weight-normal text-secondary text-sm"><span class="font-weight-bolder text-success">+$213</span> since last month</span>
                 </div>
                 <div class="col-5">
                   <div class="dropdown text-end">
-                    <a href="javascript:;" class="cursor-pointer text-secondary" id="dropdownUsers3" data-bs-toggle="dropdown" aria-expanded="false">
-                      <span class="text-xs text-secondary">6 May - 7 May</span>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end px-2 py-3" aria-labelledby="dropdownUsers3">
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Last 7 days</a></li>
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Last week</a></li>
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Last 30 days</a></li>
-                    </ul>
+                    <span class="text-xs text-secondary"><?= $annual_sales['date']; ?></span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
         <div class="col-sm-3 mt-sm-0 mt-3">
           <div class="card">
             <div class="card-body p-3 position-relative">
@@ -216,30 +213,105 @@
                 <div class="col-7 text-start">
                   <p class="text-sm mb-1 text-capitalize font-weight-bold">Active Users</p>
                   <h5 class="font-weight-bolder mb-0">
-                    34
+                    <?= $active_users; ?>
                   </h5>
-                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Pending Users</p>
+                  <!-- <p class="text-sm mb-1 text-capitalize font-weight-bold">Pending Users</p>
                   <h5 class="font-weight-bolder mb-0">
                   5
-                  </h5>
+                  </h5> -->
                 </div>
                 <div class="col-5">
                   <div class="dropdown text-end">
-                    <a href="javascript:;" class="cursor-pointer text-secondary" id="dropdownUsers3" data-bs-toggle="dropdown" aria-expanded="false">
-                      <span class="text-xs text-secondary">6 May - 7 May</span>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end px-2 py-3" aria-labelledby="dropdownUsers3">
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Last 7 days</a></li>
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Last week</a></li>
-                      <li><a class="dropdown-item border-radius-md" href="javascript:;">Last 30 days</a></li>
-                    </ul>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
+
       </div>
+
+      <!-- <div class="row mt-4">
+        <div class="col-sm-3">
+          <div class="card">
+            <div class="card-body p-3 position-relative">
+              <di v class="row">
+                <div class="col-7 text-start">
+                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Daily Sales</p>
+                  <h5 class="font-weight-bolder mb-0">
+                    $<?= number_format($daily_sales['total_sales'], 2, ".", ","); ?>
+                  </h5>
+                </div>
+                <div class="col-5">
+                  <div class="dropdown text-end">
+                    <span class="text-xs text-secondary"><?= $daily_sales['date']; ?></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        
+        <div class="col-sm-3 mt-sm-0 mt-3">
+          <div class="card">
+            <div class="card-body p-3 position-relative">
+              <div class="row">
+                <div class="col-7 text-start">
+                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Monthly Sales</p>
+                  <h5 class="font-weight-bolder mb-0">
+                    $<?= number_format($monthly_sales['total_sales'], 2, ".", ","); ?>
+                  </h5>
+                </div>
+                <div class="col-5">
+                  <div class="dropdown text-end">
+                    <span class="text-xs text-secondary"><?= $monthly_sales['date']; ?></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-3 mt-sm-0 mt-3">
+          <div class="card">
+            <div class="card-body p-3 position-relative">
+              <div class="row">
+                <div class="col-7 text-start">
+                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Annual Sales</p>
+                  <h5 class="font-weight-bolder mb-0">
+                    $<?= number_format($annual_sales['total_sales'], 2, ".", ","); ?>
+                  </h5>
+                </div>
+                <div class="col-5">
+                  <div class="dropdown text-end">
+                    <span class="text-xs text-secondary"><?= $annual_sales['date']; ?></span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-3 mt-sm-0 mt-3">
+          <div class="card">
+            <div class="card-body p-3 position-relative">
+              <div class="row">
+                <div class="col-7 text-start">
+                  <p class="text-sm mb-1 text-capitalize font-weight-bold">Pending Users</p>
+                  <h5 class="font-weight-bolder mb-0">
+                    <?= $active_users; ?>
+                  </h5>
+                </div>
+                <div class="col-5">
+                  <div class="dropdown text-end">
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div> -->
+
       <div class="row mt-4">
         <div class="col-lg-4 col-sm-6">
           <div class="card h-100">
