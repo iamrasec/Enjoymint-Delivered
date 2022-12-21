dcdde90a856e5ef601272c1f91f242037c5bc0aa
Cesar Yamuta Jr <cesaryamutajr@gmail.com>
2022-12-22 04:17:10 +0800

    debugging

--- app/Controllers/Admin/Blogs.php
+++ app/Controllers/Admin/Blogs.php
@@ -70,6 +70,7 @@ class Blogs extends BaseController {
     for($i = 0; $i < count($blog); $i++) {
       if($blog[$i]->images != null || $blog[$i]->images != '') {
         $imageIds = explode(',',$blog[$i]->images);
+        echo "<pre>".print_r($imageIds)."</pre>";die();
         $blog[$i]->images = $this->image_model->whereIn('id', $imageIds)->get()->getResult();
       }
     }
