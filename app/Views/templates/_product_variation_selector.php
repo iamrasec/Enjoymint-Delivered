104f0d55812c430913fe3de63fe049e53556d19d
Richard <126512894+RichardSabanal05@users.noreply.github.com>
2023-03-23 02:30:16 +0800

    fix: shop page error

--- app/Views/templates/_product_variation_selector.php
+++ app/Views/templates/_product_variation_selector.php
@@ -1,15 +1,15 @@
 <div class="product-variants dropdown">
-  <a class="btn dropdown-toggle variant-selector w-100 fs-6 fw-bold" href="#" role="button" id="variant-selector-<?= $product['id']; ?>" data-bs-toggle="dropdown" aria-expanded="false">
-    <span class="price fs-6 fw-bold">$<?= $product['price']; ?></span> - <span class="unit fs-6 text-lowercase fw-normal"><?= $base_product_unit; ?></span>
+  <a class="btn dropdown-toggle variant-selector w-100 fs-6 fw-bold" href="#" role="button" id="variant-selector-<?= $products[$i]['id']; ?>" data-bs-toggle="dropdown" aria-expanded="false">
+    <span class="price fs-6 fw-bold">$<?= $products[$i]['price']; ?></span> - <span class="unit fs-6 text-lowercase fw-normal"><?= $base_product_unit; ?></span>
   </a>
 
   <ul class="dropdown-menu w-100" aria-labelledby="dropdownMenuLink">
     <li class="d-none">
-      <a class="dropdown-item" href="#" data-variation-id="0" data-pid="<?= $product['id']; ?>" data-price="<?= $product['price']; ?>" data-unit="<?= $base_product_unit; ?>" data-stock="<?= $product['stocks']; ?>">
-        <div class="price fw-bold">$<span><?= $product['price']; ?></span> - <span class="unit text-lowercase fw-normal"><?= $base_product_unit; ?></span></div>
+      <a class="dropdown-item" href="#" data-variation-id="0" data-pid="<?= $products[$i]['id']; ?>" data-price="<?= $products['price']; ?>" data-unit="<?= $base_product_unit; ?>" data-stock="<?= $products[$i]['stocks']; ?>">
+        <div class="price fw-bold">$<span><?= $products[$i]['price']; ?></span> - <span class="unit text-lowercase fw-normal"><?= $base_product_unit; ?></span></div>
       </a>
     </li>
-    <?php foreach($product['variants'] as $variant): ?>
+    <?php foreach($products[$i]['variants'] as $variant): ?>
     <?php 
     switch(trim($variant->unit)) {
       case 'mg':
@@ -47,7 +47,7 @@
     } 
     ?>
     <li>
-      <a class="dropdown-item" href="#" data-variation-id="<?= $variant->id; ?>" data-pid="<?= $product['id']; ?>" data-price="<?= $variant->price; ?>" data-unit="<?= $variant_unit; ?>" data-stock="<?= $variant->stock; ?>">
+      <a class="dropdown-item" href="#" data-variation-id="<?= $variant->id; ?>" data-pid="<?= $products[$i]['id']; ?>" data-price="<?= $variant->price; ?>" data-unit="<?= $variant_unit; ?>" data-stock="<?= $variant->stock; ?>">
         <div class="price fw-bold">$<span><?= $variant->price; ?></span> - <span class="unit fw-normal"><?= $variant_unit; ?></span></div>
       </a>
     </li>
